98020fb116bb30c2c935f7f07f791fc65cf27428
JobfullyAdmin <admin@jobfully.com>
2013-12-07 00:27:09 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TummyGuard::Application.config.secret_key_base = '20a2e3830f3103afbdefa4ee1bb84f5db6d083b83c5bcebd6cfa30573effb822bd2f60e49822a7f4087034153bfd1d81fec0e0e5ba2afb451d308128ed696d60'
